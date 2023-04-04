a73afe7d35ca74659e6d3ef1b5936218dba37213
Pavlo Leinonen <leinonenko@gmail.com>
2023-04-04 09:14:29 +0300

    userController done

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -5,14 +5,15 @@
 // TODO: add function to update a user
 // TODO: add function to delete a user
 // TODO: add function to check if a token is valid
-
+import jwt from 'jsonwebtoken';
 import {Request, Response} from 'express';
 import {NextFunction} from 'express';
 import CustomError from '../../classes/CustomError';
-import {User} from '../../interfaces/User';
+import {OutputUser, User} from '../../interfaces/User';
 import userModel from '../models/userModel';
 import bcrypt from 'bcryptjs';
 import DBMessageResponse from '../../interfaces/DBMessageResponse';
+import LoginMessageResponse from '../../interfaces/LoginMessageResponse';
 
 const check = (req: Request, res: Response) => {
   res.json({message: 'Server is alive'});
@@ -69,4 +70,132 @@ const userPost = async (
   } catch (error) {
     next(new CustomError((error as Error).message, 500));
   }
+};
+
+const userPut = async (
+  req: Request<{}, {}, User>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const headers = req.headers;
+    const bearer = headers.authorization;
+    if (!bearer) {
+      next(new CustomError('No token provided', 401));
+      return;
+    }
+    const token = bearer.split(' ')[1];
+
+    const userFromToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET! as string
+    ) as OutputUser;
+
+    const user = req.body;
+    if (user.password) {
+      user.password = await bcrypt.hash(user.password, salt);
+    }
+    const result = await userModel
+      .findByIdAndUpdate(userFromToken.id, user, {
+        new: true,
+      })
+      .select('-password -role');
+
+    if (!result) {
+      next(new CustomError('User not found', 404));
+      return;
+    }
+    const response: DBMessageResponse = {
+      message: 'User updated',
+      user: {
+        user_name: result!.user_name,
+        email: result.email,
+        id: result._id,
+      },
+    };
+  } catch (error) {
+    next(new CustomError((error as Error).message, 500));
+  }
+};
+
+const userDelete = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const headers = req.headers;
+    const bearer = headers.authorization;
+    if (!bearer) {
+      next(new CustomError('No token provided', 401));
+      return;
+    }
+    const token = bearer.split(' ')[1];
+
+    const userFromToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as OutputUser;
+
+    const result = await userModel.findByIdAndDelete(userFromToken.id);
+    if (!result) {
+      next(new CustomError('User not found', 404));
+      return;
+    }
+    const response: DBMessageResponse = {
+      message: 'user deleted',
+      user: {
+        user_name: result.user_name,
+        email: result.email,
+        id: result._id,
+      },
+    };
+    res.json(response);
+  } catch (error) {
+    next(new CustomError((error as Error).message, 500));
+  }
+};
+
+const checkToken = async (req: Request, res: Response, next: NextFunction) => {
+  const headers = req.headers;
+  const bearer = headers.authorization;
+  if (!bearer) {
+    next(new CustomError('No token provided', 401));
+    return;
+  }
+  const token = bearer.split(' ')[1];
+  const userFromToken = jwt.verify(
+    token,
+    process.env.JWT_SECRET as string
+  ) as OutputUser;
+
+  const user = await userModel
+    .findById(userFromToken.id)
+    .select('-password -role');
+
+  if (!user) {
+    next(new CustomError('Token not valid', 404));
+    return;
+  }
+
+  const newToken = jwt.sign(
+    {
+      user_name: user.user_name,
+      email: user.email,
+      id: user._id,
+    },
+    process.env.JWT_SECRET as string
+  );
+
+  const message: LoginMessageResponse = {
+    message: 'Token is valid',
+    token: newToken,
+  };
+  res.json({message: 'Token is valid'});
+};
+
+export default {
+  check,
+  userListGet,
+  userGet,
+  userPost,
+  userPut,
+  userDelete,
+  checkToken,
 };
